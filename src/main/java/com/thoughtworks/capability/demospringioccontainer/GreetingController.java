fa377d1606d7524b26b6dd5f13b4c61b4e572f48
wmxl <294036748@qq.com>
2020-11-07 14:52:15 +0800

    solution1 for problem2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,34 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
+//have to be  singleton scope
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    private GreetingService greetingService;
 
     @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingController(GreetingService greetingService){
+//         this line does not matter to result
+//        this.greetingService = greetingService;
+        System.out.println("Controller: init instance...");
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return getPrototypeService().sayHi();
     }
 
+    @Lookup
+    public GreetingService getPrototypeService() {
+        return greetingService;
+    }
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
+    public GreetingService(){
+        System.out.println("Service: init instance...");
+    }
 
     String sayHi() {
         return "hello world";
